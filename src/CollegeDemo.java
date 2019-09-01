a800abe4fa0cfe3d601b83e7c577cdeffc510f27
kapilkumar8055 <kapil.kumar_cs18@gla.ac.in>
2019-09-01 19:36:17 +0530

    final commit

--- src/CollegeDemo.java
+++ src/CollegeDemo.java
@@ -3,8 +3,15 @@ public class CollegeDemo {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         College input2 = new College();
-
-
+        System.out.println("Enter Name");
+        input2.name = input.nextLine().trim();
+        System.out.println("Enter Branch");
+        input2.branch = input.nextLine().trim();
+        System.out.println("Enter course");
+        input2.course = input.nextLine().trim();
+        System.out.println("Enter University Roll No");
+        input2.universityRollNo = input.nextInt();
+        System.out.print(input2.name + "\n" + input2.course + "\n" + input2.branch + "\n" + input2.universityRollNo);
     }
 
 }
